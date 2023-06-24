f263b9fb27130ecc331febc393f66901fd2341ea
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-24 15:43:06 +0700

    feat: register

---
+++ src/user/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { hash } from "bcrypt";
+import { User } from "src/user/user.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class UserService {
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+    async findOne(options: { filter: Partial<User>}): Promise<User> {
+        return await this.userRepository.findOneByOrFail(options.filter)
+    }
+
+    async create(options: { values: Partial<User> }): Promise<User> {
+        const user = this.userRepository.create({
+            ...options.values,
+            password: await hash(options.values.password, 10)
+        })
+        
+        await this.userRepository.insert(user)
+        
+        return user
+    }
+}
